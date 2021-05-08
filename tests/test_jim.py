2c36b801449c569f4dd2b6e3ce7a9cbd5e0fa65a
sergeiKostenko <sdh1985@mail.ru>
2021-05-08 16:47:16 +0300

    created test for get_options function with default installation

--- tests/test_jim.py
+++ tests/test_jim.py
@@ -15,7 +15,12 @@ class TestClientCase(unittest.TestCase):
         test_status = "absent"
         self.assertEqual(
             type(create_presence_msg(test_user_name, test_status)), dict, "msg to send have to be in a dict form")
-        print('hello')
+
+    def test_get_options(self):
+        options = get_options(args='', options_file="../config_client.json")
+        self.assertEqual((options["DEFAULT"]["PORT"], options["DEFAULT"]["HOST"]),
+                         (7777, "127.0.0.1"),
+                         "client didn't take default port and host")
 
 
 if __name__ == '__main__':
