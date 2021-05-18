9f55e2f20875f1513d6a9e7ab1e6cafe1eb673d0
sergeiKostenko <sdh1985@mail.ru>
2021-05-18 22:23:57 +0300

    done server and clients functionality

---
+++ tests/client_tests/test_make_presence_message.py
@@ -0,0 +1,14 @@
+import unittest
+from client import make_presence_message
+
+
+class TestMakePresenceMessageFunction(unittest.TestCase):
+    def test_parse_answer_not_dict(self):
+        data = {'action': 'presence',
+                'time': 1593079094.211563,
+                'type': 'status',
+                'user': {'account_name': 'test name',
+                         'status': 'test status'}}
+        res = make_presence_message('test name', 'test status')
+        data['time'] = res['time']
+        self.assertEqual(res, data)
